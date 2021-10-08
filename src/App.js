b92a33a059a430de7febf6054a197ef66e673577
Abdou K. Sene <a_sene@salemstate.edu>
2021-10-08 17:48:59 -0400

    1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import data from './data'
-import Product from './components/Product';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-<div className="grid-container">
+    <BrowserRouter>
+          <div className="grid-container">
             <header className="row">
                 <div>
                     <a className="brand" href="/">amazona</a>
@@ -15,18 +17,12 @@ function App() {
                 </div>
             </header>
             <main>
-              <div>
-                  <div className="row center">
-                      {
-                        data.products.map(product => (
-                          <Product  key={product._id} product={product}></Product>
-                        ))
-                      }
-                    </div> 
-                </div>
+              <Route path="/product/:id" component={ProductScreen}></Route>
+              <Route path="/" component={HomeScreen} exact ></Route>
             </main>
             <footer className="row center">All rights reserved</footer>
         </div>
+    </BrowserRouter>
   );
 }
 
